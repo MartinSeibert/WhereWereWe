2eee865fd72dc42ef240f46a3f249bf5dbc85d1a
MartinSeibert <howtoemailmartin@gmail.com>
2016-09-02 20:34:11 -0400

    Register now implemented, added route for login. Next step is writing the login view

--- WhereWereWe_project/main/views.py
+++ WhereWereWe_project/main/views.py
@@ -132,3 +132,39 @@ def register(request):
 	return render(request,
 		'main/register.html',
 		{'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+def user_login(request):
+
+	# If the request is a HTTP POST, try to pull out the relevant information.
+	if request.method == 'POST':
+		# Gather the username and password provided by the user.
+		# This information is obtained from the login form.
+			# We use request.POST.get('<variable>') as opposed to request.POST['<variable>'], because the request.POST.get('<variable>') returns None if the value does not exist, while the request.POST['<variable>'] will raise key error exception
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		# Use Django's machinery to attempt to see if the username/password combination is valid - a User object is returned if it is.
+		user = authenticate(username=username, password=password)
+
+		# If we have a User object, the details are correct.
+		# If None (Python's way of representing the absence of a value), no user with matching credentials was found.
+		if user:
+			# Is the account active? It could have been disabled.
+			if user.is_active:
+				# If the account is valid and active, we can log the user in.
+				# We'll send the user back to the homepage.
+				login(request, user)
+				return HttpResponseRedirect('/main/')
+			else:
+				# An inactive account was used, so they can't log in
+				return HttpResponse("This account has been disabled or is no longer active")
+		else:
+			# Bad login details were provided
+			# To display username and password, replace the print statement with print "Invalid login details: {0}, {1}".format(username, password)			
+			print "Username or password is incorrect"
+
+		# The request is not a HTTP POST, it's likely a GET so display the login form
+	else:
+		# No context variables to pass to the template system, hence the blank dictionary object
+		return render(request, 'main/login.html', {})
+	
