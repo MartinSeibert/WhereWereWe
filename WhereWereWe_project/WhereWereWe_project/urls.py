7c92debfb6a4820d9fa15bce8a5ebe9f2fa6e48a
MartinSeibert <howtoemailmartin@gmail.com>
2016-12-04 16:29:06 -0500

    Got rid of the need to have /main/ on all of the URLS

--- WhereWereWe_project/WhereWereWe_project/urls.py
+++ WhereWereWe_project/WhereWereWe_project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^main/', include('main.urls')),               
+    url(r'^', include('main.urls')),               
 )
 
 # NOTE: this media declaration must be below the urlpatterns
