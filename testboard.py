b8ef98c43b4fc625f0cfd88645ad0f810849fbce
MartinSeibert <howtoemailmartin@gmail.com>
2018-02-04 18:36:29 -0500

    Played around on testboard, seeing if tvdbsimple will do the trick

--- testboard.py
+++ testboard.py
@@ -1,45 +1,10 @@
-#https://pythonhosted.org/pytvdbapi/examples.html#basic-usage
+import tvdbsimple as tvdb
+tvdb.KEYS.API_KEY = '491A6814F8241D30'
 
-from pytvdbapi import api
-db = api.TVDB("B43FF87DE395DF56")
-result = db.search("Attack on titan", "en")
+search = tvdb.Search()
+reponse = search.series("attack on titan")
+print search.series[0]['seriesName']
 
-# If there's a perfect match, the result will be in the 0th element
-# If not, the results will be in the order in which they may match so you could search for A or something
+show = search.series[0]
 
-print result
-
-
-show = result[0]
-
-# dir shows all of the attributes available
-print dir(show)
-
-# "Update" pulls all of the data in from the API, before you do this, the show object is incomplete. You also have to call load_actors() and load_banners to load that stuff
-
-show.update()
-
-''' Attributes: 
-['Actors', 'Airs_DayOfWeek', 'Airs_Time', 'AliasNames',
- 'ContentRating', 'FirstAired', 'Genre', 'IMDB_ID', 'Language',
- 'Network', 'NetworkID', 'Overview', 'Rating', 'RatingCount', 'Runtime',
- 'SeriesID', 'SeriesName', 'Status', 'actor_objects', 'added', 'addedBy',
- 'api', 'banner', 'banner_objects', 'fanart', 'id', 'lang', 'language',
- 'lastupdated', 'poster', 'seriesid', 'tms_wanted_old', 'zap2it_id']
- '''
-
-print(show.SeriesName)
-print(show.FirstAired)
-
-for season in show:
-	print (season.season_number)
-	print ("SEASON Length: " + str(len(season)))
-
-	for episode in season:
-		print(episode.EpisodeName)
-
-		
-# seasons are accessible through the iterators on the show object
-season_1 = show[1]
-print dir(season_1)
-print season_1.show
+print show
