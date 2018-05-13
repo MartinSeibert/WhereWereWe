10189c15f83e90ab4108aa2aab5d0a9885ba08b7
MartinSeibert <howtoemailmartin@gmail.com>
2018-05-13 19:01:49 -0400

    Changed reference to tvdbsimple to a fork where an error was fixed

--- WhereWereWe_project/main/show.py
+++ WhereWereWe_project/main/show.py
@@ -4,19 +4,21 @@ tvdb.KEYS.API_KEY = '491A6814F8241D30'
 class Show():
 
 	def __init__(self, id):
+
 		self.id = id
 		show = tvdb.Series(id)
 		response = show.info()
-		self.series = show
+
 		#self.seriesName = show.seriesName
 		self.episodes = show.Episodes.all()
 		self.posters = show.Images.poster()
-
 		maxSeason = 0
+
 		for episode in self.episodes:
 			if episode['airedSeason'] > maxSeason:
 				maxSeason = episode['airedSeason']
 		self.seriesCount = maxSeason
+		self.series = show
 
 	def printEpisodes(self):
 		for episode in self.episodes:
